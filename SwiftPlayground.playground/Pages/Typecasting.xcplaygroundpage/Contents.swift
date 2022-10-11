8976fd7de608f8342f418c79f0642e3c74f48558
jinunzzan <lovebox6607@gmail.com>
2022-10-11 19:50:02 +0900

    스위프트 문법

--- SwiftPlayground.playground/Pages/Typecasting.xcplaygroundpage/Contents.swift
+++ SwiftPlayground.playground/Pages/Typecasting.xcplaygroundpage/Contents.swift
@@ -37,3 +37,24 @@ let coffee: Coffee = Coffee(shot: 1)
 print(coffee.description) //1 shot(s) coffee
 
 let myCoffee: Americano = Americano(shot: 2, iced: false)
+print(myCoffee.description) // 2 shot(s) hot americano
+
+let yourCoffee: Latte = Latte(flavor: "green tea", shot: 3)
+print(yourCoffee.description) //3 shot(s) green tea latte
+
+print(coffee is Coffee) //true
+print(coffee is Americano)//false
+print(coffee is Latte)//false
+
+print(myCoffee is Coffee)//true
+print(yourCoffee is Coffee)//true
+
+print(myCoffee is Latte)//false
+print(yourCoffee is Latte)//true
+
+//다운캐스팅
+if let actingOne: Americano = coffee as? Americano {
+    print("This is Americano")
+} else {
+    print(coffee.description)
+}
