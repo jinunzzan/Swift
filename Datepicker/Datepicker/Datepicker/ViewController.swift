d7fbd5c6205023cbd805ab1259b7f238fd730c87
jinunzzan <lovebox6607@gmail.com>
2022-12-30 16:55:20 +0900

    DatePicker, formatter

---
+++ Datepicker/Datepicker/Datepicker/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Datepicker
+//
+//  Created by Eunchan Kim on 2022/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblCurrentTime: UILabel!
+    @IBOutlet weak var lblPickerTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+    }
+}
+
