b973a16ebb6d1cd010c85436b0d0970d32dbd985
jinunzzan <lovebox6607@gmail.com>
2022-12-30 17:16:22 +0900

    스위프트로 아이폰 앱 만들기 Datepicker 실습

--- Datepicker/Datepicker/Datepicker/ViewController.swift
+++ Datepicker/Datepicker/Datepicker/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let timeSelecteor: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
 
     @IBOutlet weak var lblCurrentTime: UILabel!
     @IBOutlet weak var lblPickerTime: UILabel!
@@ -15,6 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelecteor, userInfo: nil, repeats: true)
     }
 
 
@@ -24,6 +28,17 @@ class ViewController: UIViewController {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
         lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+    }
+    
+    @objc func updateTime() {
+//        lblCurrentTime.text = String(count)
+//        count = count + 1
+        
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
     }
 }
 
